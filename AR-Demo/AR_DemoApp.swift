8f032bcf9c9ad523812e2f0348321175dfd4a4c0
liuxiaoyue <2265015675@qq.com>
2025-06-12 10:45:09 +0200

    Initial Commit

---
+++ AR-Demo/AR_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_DemoApp.swift
+//  AR-Demo
+//
+//  Created by synapse on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AR_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR-DemoTests/AR_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_DemoTests.swift
+//  AR-DemoTests
+//
+//  Created by synapse on 12/6/25.
+//
+
+import Testing
+@testable import AR_Demo
+
+struct AR_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
